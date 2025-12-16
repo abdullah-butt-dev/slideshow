b6cf50c0a2500950899f914551aa20bcae9e069e
abdullah <230536727+abdullah-butt-dev@users.noreply.github.com>
2025-12-16 08:47:36 +0500

    Image slider completed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# webpack-template
+# Image Slider
+
+A simple image slider built using vanilla JavaScript and w3schools.
+It supports next/prev navigation, dots, and auto-play.
+
+## Features
+
+- Next and Previous buttons
+- Dot navigation
+- Auto-play slides
+- Easy to understand and customize
--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import "./styles.css";
+
+const $ = (selector) => document.querySelectorAll(selector);
+
+let slideIndex = 0;
+showSlides(slideIndex)
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  const slides = $('.slide');
+  const dots = $('.dot');
+
+  if(n > slides.length) slideIndex = 1;
+  if(n < 1) slideIndex = slides.length; 
+
+  const i = slideIndex - 1;
+
+  slides.forEach((slide) => slide.style.display = 'none')
+  dots.forEach((dot) => dot.classList.remove('active') )
+
+  slides[i].style.display = 'block';
+  dots[i].classList.add('active');
+}
+
+const navArrows = $('.nav');
+navArrows.forEach(arrow => arrow.addEventListener('click', () => {
+  arrow.classList.contains('prev') ? plusSlides(-1) : plusSlides(1)
+}))
+
+const dots = $('.dot');
+dots.forEach((dot, index) => dot.addEventListener('click', () => {
+  currentSlide(index + 1);
+}))
+
+let autoplayId = setInterval(() => plusSlides(1), 5000);
+
+const slider = document.querySelector('.slideshow-container');
+
+slider.addEventListener('mouseenter', () => clearInterval(autoplayId));
+slider.addEventListener('mouseleave', () => autoplayId = setInterval(() => plusSlides(1), 5000));
